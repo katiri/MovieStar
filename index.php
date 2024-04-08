838d2b9bc7b22050b12335f469e2fc2cb93c5123
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-04-08 09:47:37 -0300

    Criação do arquivo de estilos e teste de importação das bibliotecas que serão utilizadas

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>MovieStar</title>
+
+        <link rel="short icon" href="img/moviestar.ico">
+
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/css/bootstrap.css" integrity="sha512-nC+a1/agL6/Btk2NLY0uRsdrXxibJlYKsBhbIPKFMnVqPRtOyw1M0wd1VNPeUYjHT8YTwTNihxo/DzP++k16xQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/styles.css">        
+    </head>
+    <body>
+        <h1 class="bg-danger">Olá mundo</h1>
+        <i class="fas fa-star"></i>
+        <!-- JQuery JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+        <!-- Bootstrap JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/js/bootstrap.js" integrity="sha512-sd25Cil5f2sQtj+12OBwnBAbWsZ86Ftcu3qprCa6wFOdqXp260+cYMUHrcO0h2HPOhpGqhEbo9Lw2ihEX1wvyQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    </body>
+</html>
