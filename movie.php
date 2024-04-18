83075c54597a989533433105fe4a26db72e986ef
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-04-18 08:26:24 -0300

    Pequenas correções na página de detalhes do filme e estilização da página

--- movie.php
+++ movie.php
@@ -25,7 +25,10 @@
         $message->setMessage('O filme não foi encontrado', 'danger', 'index.php');
     }
 
-    $userOwnsMovie = true ? $userData->id === $movie->users_id : false;
+    if($userData){
+        $userOwnsMovie = true ? $userData->id === $movie->users_id : false;
+        $alreadyReview = false;
+    }
 ?>
     <div id="main-container" class="container-fluid">
         <div class="row">
@@ -48,38 +51,40 @@
             </div>
             <div id="reviews-container" class="offset-md-1 col-md-10">
                 <h3 id="reviews-title">Avaliações:</h3>
-                <div id="review-form-container" class="col-md-12">
-                    <h4>Envie sua avaliação:</h4>
-                    <p class="page-description">Preencha o formulário com a nota e o comentário sobre o filme</p>
-                    <form action="<?= $BASE_URL ?>review_process.php" method="post" id="review-form">
-                        <input type="hidden" name="type" value="create">
-                        <input type="hidden" name="movie_id" value="<?= $movie->id ?>">
-                        <div class="form-group">
-                            <label for="rating">Nota do filme:</label>
-                            <select name="rating" id="rating" class="form-control">
-                                <option value="">Selecione</option>
-                                <option value="10">10</option>
-                                <option value="9">9</option>
-                                <option value="8">8</option>
-                                <option value="7">7</option>
-                                <option value="6">6</option>
-                                <option value="5">5</option>
-                                <option value="4">4</option>
-                                <option value="3">3</option>
-                                <option value="2">2</option>
-                                <option value="1">1</option>
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label for="review">Seu comentário:</label>
-                            <textarea name="review" id="review" rows="3" class="form-control"O que você achou do filme?></textarea>
-                        </div>
-                        <input type="submit" value="Enviar comentário" class="btn card-btn">
-                    </form>
-                </div>
+                <?php if($userData && !$userOwnsMovie && !$alreadyReview): ?>
+                    <div id="review-form-container" class="col-md-12">
+                        <h4>Envie sua avaliação:</h4>
+                        <p class="page-description">Preencha o formulário com a nota e o comentário sobre o filme</p>
+                        <form action="<?= $BASE_URL ?>review_process.php" method="post" id="review-form">
+                            <input type="hidden" name="type" value="create">
+                            <input type="hidden" name="movie_id" value="<?= $movie->id ?>">
+                            <div class="form-group">
+                                <label for="rating">Nota do filme:</label>
+                                <select name="rating" id="rating" class="form-control">
+                                    <option value="">Selecione</option>
+                                    <option value="10">10</option>
+                                    <option value="9">9</option>
+                                    <option value="8">8</option>
+                                    <option value="7">7</option>
+                                    <option value="6">6</option>
+                                    <option value="5">5</option>
+                                    <option value="4">4</option>
+                                    <option value="3">3</option>
+                                    <option value="2">2</option>
+                                    <option value="1">1</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="review">Seu comentário:</label>
+                                <textarea name="review" id="review" rows="3" class="form-control"O que você achou do filme?></textarea>
+                            </div>
+                            <input type="submit" value="Enviar comentário" class="btn card-btn">
+                        </form>
+                    </div>
+                <?php endif; ?>
                 <div class="col-md-12 review">
                     <div class="row">
-                        <div class="col-md-1">
+                        <div class="col-md-1 text-center">
                             <div class="profile-image-container review-image" style="background-image: url('<?= $BASE_URL ?>img/users/user.png');"></div>
                         </div>
                         <div class="col-md-9 author-details-container">
