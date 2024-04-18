c4de43d9123f88ab2671e598f722081baa9bc883
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-04-18 09:33:43 -0300

    Lógica da edição de filme

--- movie_process.php
+++ movie_process.php
@@ -68,6 +68,71 @@
             $message->setMessage('Você precisa preencher pelo menos: título, descrição e categoria', 'danger', 'back');
         }
     }
+    else if($type === 'edit'){
+        $id = filter_input(INPUT_POST, 'id');
+        $title = filter_input(INPUT_POST, 'title');
+        $length = filter_input(INPUT_POST, 'length');
+        $category = filter_input(INPUT_POST, 'category');
+        $trailer = filter_input(INPUT_POST, 'trailer');
+        $description = filter_input(INPUT_POST, 'description');
+
+        $movie = $movieDao->findById($id);
+
+        if($movie){
+            if($movie->users_id === $user->id){
+                // Validação mínima de dados
+                if($title && $description && $category){
+                    $movie->title = $title;
+                    $movie->length = $length;
+                    $movie->category = $category;
+                    $movie->trailer = $trailer;
+                    $movie->description = $description;
+
+                    // Upload da imagem
+                    if(isset($_FILES['image']) && !empty($_FILES['image']['tmp_name'])){
+                        $image = $_FILES['image'];
+                        
+                        $imageTypes = ['image/jpeg', 'image/jpg', 'image/png'];
+                        
+                        // Checando tipo do arquivo
+                        if(in_array($image['type'], $imageTypes)){
+                            if(in_array($image['type'], ['image/jpeg', 'image/jpg'])){
+                                $imageFile = imagecreatefromjpeg($image['tmp_name']);
+                            }
+                            else{
+                                $imageFile = imagecreatefrompng($image['tmp_name']);
+                            }
+
+                            $imageName = $movie->imageGenerateName();
+
+                            imagejpeg($imageFile, './img/movies/' . $imageName, 100);
+
+                            if($movie->image){
+                                unlink('./img/movies/' . $movie->image);
+                            }
+
+                            $movie->image = $imageName;
+                        }
+                        else{
+                            $message->setMessage('Só é permitido o upload de imagens do tipo png, jpg e jpeg', 'danger', 'back');
+                            exit;
+                        }
+                    }
+
+                    $movieDao->update($movie);
+                }
+                else{
+                    $message->setMessage('Você precisa preencher pelo menos: título, descrição e categoria', 'danger', 'back');
+                }
+            }
+            else{
+                $message->setMessage('Você não cadastrou esse filme e não pode apaga-lo', 'danger', 'dashboard.php');
+            }
+        }
+        else{
+            $message->setMessage('Filme não econtrado', 'danger', 'dashboard.php');
+        }
+    }
     else if($type === 'delete'){
         $id = filter_input(INPUT_POST, 'id');
 
